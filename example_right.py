9de9d624d093aa011671b4be77fc52dde7239f19
Melissa Wu <55368740+mzwu@users.noreply.github.com>
2021-11-05 20:00:12 -0400

    read osc stream

--- example_right.py
+++ example_right.py
@@ -1,12 +1,14 @@
 import mne
 import pandas as pd
-import picard as picard
+# import picard as picard
 from mne.preprocessing import ICA
+from mne.time_frequency import stft
+from scipy.fft import fft, ifft
 
 # load data
 data = pd.read_csv("data/right_wink.csv", skiprows=0, usecols=[*range(3, 8)])
 # print(data)
-ch_names = ["EEG.AF3", "EEG.T7", "EEG.Pz", "EEG.T8", "EEG.AF4"]
+ch_names = ["AF3", "T7", "Pz", "T8", "AF4"]
 num_channels = len(ch_names)
 sfreq = 2048
 ch_types = ["eeg" for i in range(num_channels)]
@@ -14,6 +16,7 @@ ch_types = ["eeg" for i in range(num_channels)]
 # turn into mne raw object
 info = mne.create_info(ch_names=ch_names, sfreq=sfreq, ch_types=ch_types)
 raw = mne.io.RawArray(data.transpose(), info)
+print(info)
 # raw.save('data/example_right.fif')
 picks_all = [*range(num_channels)]
 
@@ -23,12 +26,30 @@ raw.plot(block=True, n_channels=num_channels, scalings=150)
 # raw.plot_psd(fmax=1024, picks=picks_all)
 
 # fit ica component
-ica = ICA(n_components=5, method='picard', random_state=97, max_iter='auto')
+ica = ICA(n_components=5, random_state=97, max_iter='auto')
 ica.fit(raw, picks=picks_all)
 raw.load_data()
 ica.plot_sources(raw, block=True, picks=picks_all, show_scrollbars=False)
 
 # high pass filter
-filt_raw = raw.copy()
-filt_raw = filt_raw.load_data().filter(picks=picks_all, l_freq=100, h_freq=None)
+filt_raw = raw.copy().load_data().filter(picks=picks_all, l_freq=25, h_freq=None)
 filt_raw.plot(block=True, n_channels=num_channels, scalings=150)
+
+# fit ica component
+# ica = ICA(n_components=5, method='picard', random_state=97, max_iter='auto')
+ica = ICA(n_components=5, random_state=97, max_iter='auto')
+ica.fit(filt_raw, picks=picks_all)
+filt_raw.load_data()
+ica.plot_sources(filt_raw, block=True, picks=picks_all, show_scrollbars=False)
+
+# visualize 10-20 eeg labeling system
+montage = mne.channels.make_standard_montage(kind='standard_1020')
+raw_1020 = raw.copy().set_montage(montage)
+fig = montage.plot(kind='3d')
+fig.gca().view_init(azim=70, elev=15)  # set view angle
+montage.plot(kind='topomap')
+print(montage)  # 94 channels
+print(raw_1020.get_montage())  # 5 channels
+ica.plot_sources(filt_raw, block=True, picks=picks_all, show_scrollbars=False)
+# ica.plot_properties(raw_1020, picks=[1])
+# ica.plot_components(raw, picks=1)
---
+++ osc_read.py
@@ -0,0 +1,103 @@
+import argparse
+import time
+import pandas as pd
+
+from pythonosc import dispatcher
+from pythonosc import osc_server
+
+PORT_NUMBER = 8000
+IP_DEFAULT = "127.0.0.1"
+
+start = time.process_time()
+metrics = ["/fac/eyeAct/lookL", "/fac/eyeAct/lookR", "/fac/eyeAct/blink", "/fac/eyeAct/winkL", "/fac/eyeAct/winkR",
+           "/fac/uAct/neutral", "/fac/uAct/frown", "/fac/uAct/surprise", "/fac/lAct/neutral", "/fac/lAct/clench",
+           "/fac/lAct/laugh", "/fac/lAct/smile", "/fac/lAct/smirkLeft", "/fac/lAct/smirkRight", "/com/neutral",
+           "/com/push", "/com/pull", "/com/left", "/com/right", "/com/lift", "/com/drop", "/com/rotateLeft",
+           "/com/rotateRight", "/com/rotateClockwise", "/com/rotateCounterClockwise", "/com/rotateForwards",
+           "/com/rotateReverse", "/com/disappear", "/met/foc", "/met/int", "/met/rel", "/met/str", "/met/exc",
+           "/met/eng", "/met/cognitiveStress", "/met/visualAttention", "/met/auditoryAttention"]
+num = len(metrics)
+lookup = {val:idx for idx, val in enumerate(metrics)}
+data = {}  # time: [metric values (ordered)]
+
+
+def filter_handler(address, *args):
+    print(f"{address}: {args}")
+    curr = time.process_time() - start
+    print(curr)
+    # add time + metric value to data
+    if curr not in data:
+        data[curr] = [0 for i in range(num)]
+    val = args
+    while not isinstance(val, int) and not isinstance(val, float):
+        val = val[0]
+    data[curr][lookup[address]] = val
+    # recording done, export data to csv
+    if curr > 0.5:
+        # columns: time, metrics
+        df = pd.DataFrame.from_dict(data, orient='index', columns=metrics)
+        df.to_csv('data/blink_110521.csv')
+        exit()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip",
+                        default=IP_DEFAULT, help="The ip to listen on")
+    parser.add_argument("--port",
+                        type=int, default=PORT_NUMBER, help="The port to listen on")
+    args = parser.parse_args()
+
+    dispatcher = dispatcher.Dispatcher()
+
+    # === Facial Expressions Eye
+    dispatcher.map("/fac/eyeAct/lookL", filter_handler)
+    dispatcher.map("/fac/eyeAct/lookR", filter_handler)
+    dispatcher.map("/fac/eyeAct/blink", filter_handler)
+    dispatcher.map("/fac/eyeAct/winkL", filter_handler)
+    dispatcher.map("/fac/eyeAct/winkR", filter_handler)
+
+    # === Facial Expressions Upperface
+    dispatcher.map("/fac/uAct/neutral", filter_handler, time.process_time() - start)  # WORK IN PROGRESS
+    dispatcher.map("/fac/uAct/frown", filter_handler)
+    dispatcher.map("/fac/uAct/surprise", filter_handler)
+
+    # === Facial Expressions Lowerface
+    dispatcher.map("/fac/lAct/neutral", filter_handler, time.process_time() - start)
+    dispatcher.map("/fac/lAct/clench", filter_handler)
+    dispatcher.map("/fac/lAct/laugh", filter_handler)
+    dispatcher.map("/fac/lAct/smile", filter_handler)
+    dispatcher.map("/fac/lAct/smirkLeft", filter_handler)
+    dispatcher.map("/fac/lAct/smirkRight", filter_handler)
+
+    # === Mental Commands
+    dispatcher.map("/com/neutral", filter_handler)
+    dispatcher.map("/com/push", filter_handler)
+    dispatcher.map("/com/pull", filter_handler)
+    dispatcher.map("/com/left", filter_handler)
+    dispatcher.map("/com/right", filter_handler)
+    dispatcher.map("/com/lift", filter_handler)
+    dispatcher.map("/com/drop", filter_handler)
+    dispatcher.map("/com/rotateLeft", filter_handler)
+    dispatcher.map("/com/rotateRight", filter_handler)
+    dispatcher.map("/com/rotateClockwise", filter_handler)
+    dispatcher.map("/com/rotateCounterClockwise", filter_handler)
+    dispatcher.map("/com/rotateForwards", filter_handler)
+    dispatcher.map("/com/rotateReverse", filter_handler)
+    dispatcher.map("/com/disappear", filter_handler)
+
+    # === Performance Metrics
+    dispatcher.map("/met/foc", filter_handler)
+    dispatcher.map("/met/int", filter_handler)
+    dispatcher.map("/met/rel", filter_handler)
+    dispatcher.map("/met/str", filter_handler)
+    dispatcher.map("/met/exc", filter_handler)
+    dispatcher.map("/met/eng", filter_handler)
+    dispatcher.map("/met/cognitiveStress", filter_handler)
+    dispatcher.map("/met/visualAttention", filter_handler)
+    dispatcher.map("/met/auditoryAttention", filter_handler)
+
+    server = osc_server.ThreadingOSCUDPServer(
+        (args.ip, args.port), dispatcher)
+    print("Serving on {}".format(server.server_address))
+    server.serve_forever()
