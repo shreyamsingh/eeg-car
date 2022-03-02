f6822e302b9608ad301cc36504da3079570e8a29
Shreya Singh <33792510+shreyamsingh@users.noreply.github.com>
2022-03-02 14:46:18 -0500

    update dashboard

--- python/dashboard_read.py
+++ python/dashboard_read.py
@@ -1,13 +1,16 @@
 # run with write_to_file.py
 import dash
 from dash import dcc
-from dash import html
+from dash import html, callback_context
 from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
+import serial
 
 app = dash.Dash(__name__)
+#df = pd.read_csv('eeg-car/melissa_push.csv')
 df = pd.read_csv('data.csv')
+ser = serial.Serial(port='COM3', baudrate=9600, timeout=.1)
 
 app.layout = html.Div(children=[
    html.H1('EEG DATA'),
@@ -20,7 +23,19 @@ app.layout = html.Div(children=[
             interval=1000, # in milliseconds
             n_intervals=0
          )
-      ], style={'border':'1px solid', 'border-radius': 10, 'backgroundColor':'#FFFFFF'})
+      ], style={'border':'1px solid', 'border-radius': 10, 'backgroundColor':'#FFFFFF'}),
+   html.Div(children=[
+      html.H2('Car Controls'),
+      html.Div(children=[
+         html.Button('Left', id='left', n_clicks=0),
+         html.Button('Right', id='right', n_clicks=0),
+         html.Button('Forward', id='forward', n_clicks=0),
+         html.Button('Backward', id='backward', n_clicks=0),
+         html.button('Stop', id='stop', n_clicks=0),
+         html.Div(id='car-controls')
+      ])
+   ])
+
 ])
 
 
@@ -30,14 +45,29 @@ app.layout = html.Div(children=[
    #Input('interval-component', 'n_intervals')
 )
 def update_graph(n, value):
-   current = pd.read_csv('data.csv')
-   print(current)
+   #print(value)
+   current = df.iloc[0:n]
    fig = px.line(current, x='time', y=value).update_traces(mode='lines+markers')
    fig.update_layout({
       'paper_bgcolor': 'rgba(0,0,0,0)'
    })
-   fig.update_xaxes(range=[max(0, current['time'].max() - 0.5), current['time'].max()])
+   #fig.update_xaxes(range=[max(0, current['time'].max() - 0.5), current['time'].max()])
+   #fig.update_xaxes(range=[0,0.5])
+   fig.update_yaxes(range=[-0.2,1.2])
    return fig
+
+@app.callback(
+   Output('car-controls', 'children'),
+   Input('left', 'n_clicks'),
+   Input('right', 'n_clicks'),
+   Input('forward', 'n_clicks'),
+   Input('backward', 'n_clicks'),
+   Input('stop', 'n_clicks')
+)
+def update_car_controls(left, right, forward, backward):
+   changed_id = [p['prop_id'] for p in callback_context.triggered][0]
+   #print(changed_id[:-9])
+   ser.write(changed_id[0].encode())
 
 if __name__ == '__main__':
    app.run_server(debug=True)
