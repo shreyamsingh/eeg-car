323e4950f16ecc09681342a8dbb8cc2d119e0bc4
Melissa Wu <55368740+mzwu@users.noreply.github.com>
2022-03-09 13:34:15 -0500

    ensure message is valid

--- python/serial_read.py
+++ python/serial_read.py
@@ -4,11 +4,14 @@ import pandas as pd
 # open serial port
 ser = serial.Serial(port='COM3', baudrate=9600, timeout=.1)
 
+commands = {'f', 'b', 'l', 'r', 's'}
+
 while True:
     # read csv
     df = pd.read_csv('serial.csv')
     # get last command
     msg = df.loc[len(df)-1].at['msg']
     # write message to serial port
-    ser.write(msg.encode())
+    if msg in commands:
+        ser.write(msg.encode())
     time.sleep(0.01)
